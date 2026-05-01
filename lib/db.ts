193f65452b30e8938144f69e0e85a17587deaf8f
Gabriel vieira <gabrielvieira7781@gmail.com>
2026-05-01 20:35:06 -0300

    Atualização de deploy 2

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,24 @@
 // lib/db.ts
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL as string,
+      },
+    },
+  });
+};
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
+  globalThis.prismaGlobal = prisma;
 }
